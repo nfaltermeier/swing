df4a7d133056d4bce7e2d1f81ee05f9cba2d83de
18ljones <ljones19992018@gmail.com>
2021-04-23 15:10:37 -0500

    Added tutorial text to main menu

--- Screens/MainMenu.cs
+++ Screens/MainMenu.cs
@@ -17,7 +17,13 @@ namespace Swing.Screens
             MainGame.Instance.RunTime = TimeSpan.Zero;
 
             float halfScreenWidth = MainGame.Instance.DisplayWidth / 2f;
+            float halfScreenHeight = MainGame.Instance.DisplayHeight / 2f;
 
+            string tutorial_text = "Movement - WASD\nJump - Space (Hold for higher jump)\nInteract with menu - Mouse\nPause - Escape\n";
+            string objective_text = "Get to the goal (green square)\nas fast as you can to beat the level!\nGood luck!";
+
+            Instantiate(new TextRenderer(new Vector2(halfScreenWidth / 2, halfScreenHeight), tutorial_text, TextRenderer.Style.Medium));
+            Instantiate(new TextRenderer(new Vector2(halfScreenWidth * 1.5f, halfScreenHeight), objective_text, TextRenderer.Style.Medium));
             Instantiate(new TextRenderer(new Vector2(halfScreenWidth, 200), "Swing", TextRenderer.Style.Large));
             Instantiate(new Button(new Vector2(halfScreenWidth, 400), "Play")).Activated += Play_Activated;
             Instantiate(new SettingsButton<MainMenu>(new Vector2(halfScreenWidth, 600)));
